ff7f788c5173f3937d34ac05afbecc0db5904245
Reeyou <ryu993830137@163.com>
2018-09-01 00:40:53 +0800

    update

--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-
-const getters = {
-  friend: (state) => {
-    return state.data.friends.filter(user => user._id === state.personalId)[0]
-    //getters筛选数据，即返回符合当前friends中_id值与personalId相等的数组，由于数组只有一位，获取数组第一位的json数据
-  }
-}
-
-export default getters
